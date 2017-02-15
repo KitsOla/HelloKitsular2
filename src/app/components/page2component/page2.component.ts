990e29096e6a727c2cc787bd351cefc7f29b1e3c
KitsOla <ola.nilsson@kits.se>
2017-02-15 11:48:43 +0100

    Components
    Created example pages as components

---
+++ src/app/components/page2component/page2.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from './app/user';
+
+const USERS : User[] = [
+   	{
+   		id: 1,
+   		firstname: 'Jens',
+   		lastname: 'Olsson'
+   	},
+   	{
+   		id: 2,
+   		firstname: 'Kalle',
+   		lastname: 'Bengtsson'
+   	},
+   	{
+   		id: 3,
+   		firstname: 'Harald',
+   		lastname: 'Rolandsson'
+   	}
+];
+
+@Component({
+  selector: 'kits-page2',
+  templateUrl: './app/components/page2component/page2.component.html',
+  styleUrls: [ './app/components/page2component/page2.component.css' ]
+})
+
+export class Page2Component implements OnInit{
+ 
+ 	users = USERS;
+
+   ngOnInit(): void {
+     //this.getNames();
+   }
+
+   getNames(): void{
+   		//users = USERS;
+   }
+}
